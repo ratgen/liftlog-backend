94c2ee1d7ba4b90428c8b2cdb18424e7ba922543
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-10-13 17:10:39 +0200

    Working displaying of received JSON data

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,42 @@
+use rocket::serde::json::Json;
+use rocket::serde::{Serialize, Deserialize};
+use std::vec::Vec;
+
+
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+#[derive(Serialize, Deserialize, Debug)]
+struct Repetition {
+    repetitions: u8,
+    weight: u8
+}
+impl ToString for Repetition {
+    fn to_string(&self) -> String {
+        format!("{}, {}", self.repetitions, self.weight)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Exercise {
+    name: String,
+    set: Vec<Repetition>
+}
+impl ToString for Exercise {
+    fn to_string(&self) -> String {
+        format!("{}, {:#?}",self.name, self.set)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Workout<'r> {
+    title: &'r str,
+    dueDate: u64,
+    exerciseList: Vec<Exercise>
+}
+
+#[post("/workout", format = "json", data = "<workout>")]
+fn hello(workout : Json<Workout> ) -> String {
+    return format!("Workout is {:?}", workout);
 }
 
 #[launch]
